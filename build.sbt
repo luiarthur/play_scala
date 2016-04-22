883f183587b11481aa5cfc57dedd5f1dd21783da
Arthur Lui <luiarthur@gmail.com>
2016-04-22 16:14:01 -0700

    yes

---
+++ build.sbt
@@ -0,0 +1,18 @@
+name := "playing"
+
+libraryDependencies  ++= Seq(
+  "org.scalanlp" %% "breeze" % "0.12",
+  "org.scalanlp" %% "breeze-natives" % "0.12",
+  "org.scalanlp" %% "breeze-viz" % "0.12"
+)
+
+resolvers ++= Seq(
+  "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
+  "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/"
+)
+
+// Scala 2.9.2 is still supported for 0.2.1, but is dropped afterwards.
+scalaVersion := "2.11.8"
+
+unmanagedJars in Compile += file("/home/luiarthur/Libs/jvmr_2.11-2.11.2.1.jar")
+scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
---
+++ main.scala
@@ -0,0 +1,31 @@
+import org.ddahl.jvmr.RInScala
+import breeze.stats.distributions._
+import breeze.linalg._
+ 
+object ScalaToRTest {
+  def main(args: Array[String]) = {
+ 
+    // first simulate some data consistent with a Poisson regression model
+    val x = Uniform(50,60).sample(1000)
+    val eta = x map { xi => (xi * 0.1) - 3 }
+    val mu = eta map { math.exp(_) }
+    val y = mu map { Poisson(_).draw }
+     
+    // call to R to fit the Poission regression model
+    val R = RInScala() // initialise an R interpreter
+    R.x = x.toArray // send x to R
+    R.y = y.toArray // send y to R
+    R.eval("mod <- glm(y~x,family=poisson())") // fit the model in R
+    // pull the fitted coefficents back into scala
+    val beta = DenseVector[Double](R.toVector[Double]("mod$coefficients"))
+
+
+    R.eval("library(maps)") // fit the model in R
+    R.eval("map('state')") // fit the model in R
+    R.eval("my_p <- function(x) plot(x,fg='grey',bty='n')") // fit the model in R
+    R.eval("my_p(rnorm(100))") // fit the model in R
+ 
+    // print the fitted coefficents
+    println(beta)
+  }
+}
