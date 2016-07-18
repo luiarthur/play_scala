2afdd96c16a879cefca84ce895fbef102f7501d6
Arthur Lui <luiarthur@gmail.com>
2016-07-17 19:37:49 -0700

    yes

--- scala99/ListExercises.scala
+++ scala99/ListExercises.scala
@@ -22,10 +22,12 @@ palindrome.reverse == palindrome
 
 //07: flatten a list
 val bumpyList = List(1,List(1,2,3),List(List(4,3),List(2,5)))
-bumpyList flatMap {
-  case ms: List[_] => flat(ms)
+//def flatten[T](ls: List[T]): List[Any] = ls flatMap {
+def flatten[T](ls: List[T]): List[Any] = ls flatMap {
+  case ms: List[_] => flatten(ms)
   case e => List(e)
 }
+flatten(bumpyList)
 
 //08: Eliminate consecutive dups
 val redundant = List(3,3,3,1,2,2,2,2,2,4,4,4,3)
@@ -48,3 +50,8 @@ pack(redundant)
 def encode[T](ls: List[T]): List[(Int,T)] = pack(ls) map { x => (x.size, x.head) }
 encode(redundant)
 encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) == List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+
+//11: Modified run-length encoding of list (no dups => just elem in list)
+//def encodeModified[T](ls: List[T]): List[Any] = encode(ls) map { x => if (x._1 > 1) x else x._2}
+def encodeModified[T](ls: List[T]): List[Either[T, (Int,T)]] = encode(ls) map { x => if (x._1 > 1) Right(x) else Left(x._2)}
+encodeModified(redundant)
