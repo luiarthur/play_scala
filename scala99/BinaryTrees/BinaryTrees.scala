a73ed82b7d2e539b45e9c49c3c0986b1f385dbb7
Arthur Lui <luiarthur@gmail.com>
2016-08-04 17:51:47 -0700

    yes

--- scala99/BinaryTrees/BinaryTrees.scala
+++ scala99/BinaryTrees/BinaryTrees.scala
@@ -1,4 +1,5 @@
 // immutable updatable tree
+// test 1
 object BinaryTrees {
 
   object Node {
@@ -26,3 +27,34 @@ t.isLeaf
 val t2 = Node(1, Node(2), Node(3, Node(4), Node(5) ))
 
 t2.updateLeaf(5,100)
+
+
+
+
+// test 2
+sealed abstract class Tree[+T] { def addValue[U >: T <% Ordered[U]](x: U): Tree[U] }
+
+case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
+  def addValue[U >: T <% Ordered[U]](x: U) =
+    if (x < value) Node(value, left.addValue(x), right)
+    else Node(value, left, right.addValue(x))
+}
+object Node { def apply[T](value: T): Node[T] = Node(value, End, End) }
+case object End extends Tree[Nothing] { def addValue[U <% Ordered[U]](x: U) = Node(x) }
+
+
+// test 3
+
+object Tree { def apply[T](elem: T): Tree[T] = Tree(elem, null, null) }
+case class Tree[T](elem: T, left: Tree[T], right: Tree[T]) {
+  def isLeaf = (left,right) match {case (null,null) => true; case _ => false}
+}
+
+object TI { 
+  def apply(e: Int): TI = new TI(e, null, null) 
+  def apply(e: Int, l: TI, r: TI): TI = new TI(e, l, r) 
+}
+class TI(elem: Int, left: TI, right: TI) extends Tree[Int](elem,left,right)
+
+val t = TI(1,TI(2),TI(3))
+t.copy(left=TI(100))
