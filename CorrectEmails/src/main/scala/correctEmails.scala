ed4a6e85d4c315e7b4e01540d9019442f1f91dba
Arthur Lui <luiarthur@gmail.com>
2016-11-03 10:55:05 -0700

    completed correctEmails.scala

--- CorrectEmails/src/main/scala/correctEmails.scala
+++ CorrectEmails/src/main/scala/correctEmails.scala
@@ -1,15 +1,19 @@
 package CorrectEmails
 
 object Fun {
-  def correctEmails(pattern: String): Int = {
-    val subs = List('a','b','c','d','e','@','.')
-    val rgx = """\A[a-e|?]+(\.*[a-e|?])*[@|?][a-e|?]+[.|?][a-e|?]+(\.*[a-e|?]+)*\z""".r
+  def correctEmails(pattern: String, j: Int = 0): Int = {
+    //val subs = List('a','b','c','d','e','@','.')
+    val subs = List('a','@','.')
+    val rgx = """\A[a-e|?]+(\.?[a-e|?]+)*[@|?][a-e|?]+[.|?][a-e|?]+(\.?[a-e|?]+)*\z""".r
     def validEmail(s: String) = rgx.findFirstIn(s).toArray.length>0
     
     if (validEmail(pattern)) {
-      val i = pattern.indexOf('?')
+      val i = pattern.indexOf('?', j)
       if (i > -1) {
-        subs.par.map(c => correctEmails(pattern.updated(i,c))).sum
+        subs.map(c => 
+          (if (c=='a') 5 else 1) * 
+          correctEmails(pattern.updated(i,c), i+1)
+        ).sum
       } else {
         1
       }
