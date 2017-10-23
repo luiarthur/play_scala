27715f33625ace6c2f8cf7d639a683c0cbbf1be6
Arthur Lui <luiarthur@gmail.com>
2017-10-23 09:17:19 -0700

    yes

--- dsl/bayes/BayesDsl.scala
+++ dsl/bayes/BayesDsl.scala
@@ -1,4 +1,6 @@
-trait BayesDsl {
+import scala.language.implicitConversions
+
+abstract class BayesDsl {
   sealed abstract class Type
   object Int extends Type { override def toString = "Int"}
   object Double extends Type { override def toString = "Double"}
@@ -18,12 +20,20 @@ trait BayesDsl {
   def generateCode() = {
     ???
   }
+
+  implicit def distributed(sym:Symbol) = {
+    println("Hi")
+  }
 }
+
 
 object myMCMC extends BayesDsl {
   val Data = Map('y -> Vec(Int, 'N), 
                  'X -> Mat(Double, 'N, 'K),
                  'Z -> Arr(Mat(Int, 'J, 'N_i), 'I))
+
+  //'y.distributed
+  //distributed('y)
 
   def main(args:Array[String]=Array()) = {
     Data.foreach{ i => println(i._2) }
